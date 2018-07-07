ac25c00f4539f1613423364c53d95318877e69ab
VKuosma <vibe96@gmail.com>
2018-07-07 15:17:49 +0300

    Changed server port.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 var express = require('express');
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 1337;
 
 app.listen(port);
 app.use(express.static('public'));
