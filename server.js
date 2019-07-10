789538ddecf97a48a2f63f6e74f77a45b472e67e
Ville Kuosmanen <vibe96@gmail.com>
2019-07-10 20:07:15 +0100

    Update server to serve correct files.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
-const http = require('http');
 var express = require('express');
+const path = require('path');
 var app = express();
+
 const port = process.env.PORT || 1337;
 console.log("Starting on port " + port);
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 app.listen(port);
-app.use(express.static("public"));
