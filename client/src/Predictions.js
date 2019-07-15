310eb2aee9e7790fa7629c72614ca0e4fd18482c
Ville Kuosmanen <vibe96@gmail.com>
2019-07-15 23:14:31 +0100

    Add ability to see predictions from the past

--- client/src/Predictions.js
+++ client/src/Predictions.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select';
 
 import DriverList from './DriverList';
 import { PredictionsGraph } from './PredictionsGraph';
@@ -15,9 +16,9 @@ export default class Predictions extends React.Component {
             order: [],
             predictions: {},
             selectedDriverId: null,
-            raceName: "",
-            raceYear: null,
-            racesIndex: {}
+            gpTitle: "",
+            racesList: {},
+            changeQualiExpanded: false
         };
 
         //Fetch index file, sort the years and races in it
@@ -33,15 +34,20 @@ export default class Predictions extends React.Component {
                 const years = Object.keys(res).sort(collator.compare);
                 //For each year:
                 console.log("Above loop" + years);
+                const racesList = [];
                 for (let i = 0; i < years.length; i++) {
                     const races = Object.keys(res[years[i]]).sort(collator.compare);
+                    races.map(id => (
+                        racesList.push({ value: id, label: `${years[i]} ${res[years[i]][id]}` })
+                    ));
                     console.log(races);
 
                     //Select most recent race.
                     console.log("In loop");
                     mostRecentId = races[races.length - 1];
+                    
                 }
-                this.setState({ racesIndex: res });
+                this.setState({ racesList });
                 fetch('data/' + mostRecentId + '.json')
                     .then(res => {
                         return res.json();
@@ -51,8 +57,7 @@ export default class Predictions extends React.Component {
                             drivers: res["drivers"],
                             order: res["order"],
                             predictions: res["predictions"],
-                            raceName: res["name"],
-                            raceYear: res["year"],
+                            gpTitle: `${res["year"]} ${res["name"]}`,
                         });
                     });
             });
@@ -63,12 +68,45 @@ export default class Predictions extends React.Component {
         this.setState({ selectedDriverId: did });
     };
 
+    onToggleQualiOptionsSelected = () => {
+        this.setState({changeQualiExpanded: !this.state.changeQualiExpanded});
+    }
+
+    onNewQualiSelected = selection => {
+        console.log(selection);
+        fetch('data/' + selection.value + '.json')
+            .then(res => {
+                return res.json();
+            })
+            .then(res => {
+                this.setState({
+                    drivers: res["drivers"],
+                    order: res["order"],
+                    predictions: res["predictions"],
+                    gpTitle: `${res["year"]} ${res["name"]}`,
+                });
+            });
+    }
+
     render() {
         const predictionsForDriver = this.state.selectedDriverId ? this.state.predictions[this.state.selectedDriverId] : {};
         const driverColor = this.state.selectedDriverId ? this.state.drivers[this.state.selectedDriverId].color : null; 
         return (
             <div>
-                <div className="qualiHeader">{this.state.raceYear} {this.state.raceName} - Qualifying predictions</div>
+                <div className="qualiHeader">
+                    <span className="qualiHeaderText">{this.state.gpTitle} - Qualifying predictions</span>
+                    <button onClick={this.onToggleQualiOptionsSelected} className='toggleQualiOptionsButton'>
+                        { this.state.changeQualiExpanded ?  'Cancel' : 'Change GP' }
+                    </button>
+                    {this.state.changeQualiExpanded ?<div>
+                        <Select
+                            className='qualiOptionsSelect'
+                            value={this.state.gpTitle}
+                            options={this.state.racesList}
+                            onChange={this.onNewQualiSelected}
+                        />
+                    </div> : null}
+                </div>
                 <div className="mainContainer">
                     <DriverList 
                         rowClicked={this.handleDriverSelection}
@@ -81,7 +119,7 @@ export default class Predictions extends React.Component {
                                 style={{color: this.state.drivers[this.state.selectedDriverId].color}}>
                                 {this.state.drivers[this.state.selectedDriverId].name}
                             </div> : 
-                            <div className="selectedDriverText">Choose your driver:</div>
+                            <div className="selectedDriverText" style={{color: '#888888'}}>Choose your driver:</div>
                         }
                         <PredictionsGraph
                             predictions={predictionsForDriver}
