8f284a5457caa03530dc645e48520f84c18a5cfc
Ville Kuosmanen <vibe96@gmail.com>
2019-08-18 09:40:52 +0100

    Updates

---
+++ precache-manifest.d2498335a34418db3c65e835757cfbb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3eaf68e02872a60a5bca1e79f1e63b3",
+    "url": "/F1PredictWeb/index.html"
+  },
+  {
+    "revision": "f2ccbf3ae842a30ef9f4",
+    "url": "/F1PredictWeb/static/css/main.102662c7.chunk.css"
+  },
+  {
+    "revision": "31a94ed8503de7783b5e",
+    "url": "/F1PredictWeb/static/js/2.9aaa3796.chunk.js"
+  },
+  {
+    "revision": "f2ccbf3ae842a30ef9f4",
+    "url": "/F1PredictWeb/static/js/main.acc67523.chunk.js"
+  },
+  {
+    "revision": "35e9c80d8b2dfd57de50",
+    "url": "/F1PredictWeb/static/js/runtime~main.2d340179.js"
+  }
+]);
