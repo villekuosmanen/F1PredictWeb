9146742744a689d225f8625b4d227476ab6e52e6
Ville Kuosmanen <vibe96@gmail.com>
2020-09-29 20:09:06 +0300

    Updates

---
+++ precache-manifest.608d5592a009d81e80fd37518c752085.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ccf324c5ddef421d87331d54d8bc0c2",
+    "url": "/F1PredictWeb/index.html"
+  },
+  {
+    "revision": "38e9906c399221318232",
+    "url": "/F1PredictWeb/static/css/main.f7f3891c.chunk.css"
+  },
+  {
+    "revision": "36ad96d30ad1a37ac8c6",
+    "url": "/F1PredictWeb/static/js/2.7f6a2a5b.chunk.js"
+  },
+  {
+    "revision": "60037e8887f425d039bd63775efa9319",
+    "url": "/F1PredictWeb/static/js/2.7f6a2a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38e9906c399221318232",
+    "url": "/F1PredictWeb/static/js/main.b6ade5fb.chunk.js"
+  },
+  {
+    "revision": "a9749ddcf134825128a5",
+    "url": "/F1PredictWeb/static/js/runtime-main.40696bf1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/F1PredictWeb/precache-manifest.aa6ab93d6cae94c5d3758a15a1a39c62.js"
+  "/F1PredictWeb/precache-manifest.608d5592a009d81e80fd37518c752085.js"
 );
 
 self.addEventListener('message', (event) => {
